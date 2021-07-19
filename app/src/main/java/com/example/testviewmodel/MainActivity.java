08b3e0992b9ca2fe4f996f2bd07221f29b786b49
SoleneMG <solene.michelgrosjean@gmail.com>
2021-07-19 10:03:39 +0200

    increment

--- app/src/main/java/com/example/testviewmodel/MainActivity.java
+++ app/src/main/java/com/example/testviewmodel/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.testviewmodel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView count;
+    private ViewModel viewModel = new ViewModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        count = findViewById(R.id.count);
+    }
+
+    public void onClickIncrementButton(View view) {
+        int countInt = Integer.parseInt((String) count.getText());
+        int result = viewModel.increment(countInt);
+        count.setText(String.valueOf(result));
+
     }
 }
---
+++ app/src/main/java/com/example/testviewmodel/ViewModel.java
@@ -0,0 +1,8 @@
+package com.example.testviewmodel;
+
+public class ViewModel {
+
+    public int increment(int i){
+        return i+1;
+    }
+}
